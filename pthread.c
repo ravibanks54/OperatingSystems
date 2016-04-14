233320173de8bedd69b7ec4a51dd2fea8b8d4c42
Ravi Bhankharia <rab302@pascal.cs.rutgers.edu>
2016-04-14 11:58:02 -0400

    more minor changes

--- pthread.c
+++ pthread.c
@@ -27,3 +27,20 @@ int pthread_exit(void *retval){
 }
 
 */
+
+/*
+int pthread_mutex_destroy(pthread_mutex_t *mutex){
+	return 0;
+}
+
+int pthread_mutex_init(pthread_mutex_t *mutex, const pthread_mutexattr_t *attr){
+	return 0;
+}
+
+int pthread_mutex_lock(pthread_mutex_t *mutex){
+	return 0;
+}
+int pthread_mutex_unlock(pthread_mutex_t *mutex){
+	return 0;
+}
+*/
